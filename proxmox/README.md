f364ba76e3c12a5dffedd73ae6d4356f18257e32
Laurin Streng <laurinstreng1@gmail.com>
2025-02-27 23:18:25 +0100

    add proxmox

---
+++ proxmox/README.md
@@ -0,0 +1,8 @@
+## Tokens
+
+To Configure the proxmox provider, the following shoud be placed in the prod.tfvars file:
+
+```
+virtual_environment_api_token = ""
+virtual_environment_endpoint  = ""
+```
---
+++ proxmox/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.73.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.virtual_environment_endpoint
+  api_token = var.virtual_environment_api_token
+}
