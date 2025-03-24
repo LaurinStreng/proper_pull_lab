9b133efc5fa9529b963d54003d5176f4dc881bbb
Laurin Streng <laurinstreng1@gmail.com>
2025-03-24 22:41:35 +0100

    add ansible pinut host

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-ansible-playbook site.yml --inventory-file ./hosts.yml --private-key ./ansible_deployment_key
+HOST=${1}
+
+if [ -z "$HOST" ]; then
+  echo "Please provide a Host or use all to deploy to all hosts"
+  exit 1
+fi
+
+ansible-playbook site.yml --inventory-file ./hosts.yml --private-key ./ansible_deployment_key --limit "$HOST"
